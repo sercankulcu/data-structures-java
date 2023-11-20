12642c94b20baf4172d12dd942e9fd52cbcdecee
Sercan Külcü <sercankulcu@gmail.com>
2023-11-20 19:34:46 +0300

    added some functions

--- Ders02/src/DiziTersCevirme.java
+++ Ders02/src/DiziTersCevirme.java
@@ -1,15 +1,23 @@
+import java.util.Stack;
 
 public class DiziTersCevirme {
-	
+
 	public static void main(String[] args) {
-		
+
 		int[] dizi = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 		System.out.println("Orijinal Dizi:");
 		diziYazdir(dizi);
 
 		int[] tersDizi = diziTersCevir(dizi);
+		System.out.println("\nTers Çevrilmiş Dizi:");
+		diziYazdir(tersDizi);
 
+		tersDizi = diziTersCevir2(dizi);
+		System.out.println("\nTers Çevrilmiş Dizi:");
+		diziYazdir(tersDizi);
+
+		tersDizi = diziTersCevir3(dizi);
 		System.out.println("\nTers Çevrilmiş Dizi:");
 		diziYazdir(tersDizi);
 	}
@@ -17,12 +25,36 @@ public class DiziTersCevirme {
 	public static int[] diziTersCevir(int[] dizi) {
 
 		int[] tersDizi = new int[dizi.length];
-
 		for (int i = 0; i < dizi.length; i++) {
 			tersDizi[i] = dizi[dizi.length - 1 - i];
 		}
 
 		return tersDizi;
+	}
+
+	public static int[] diziTersCevir2(int[] dizi) {
+
+		int uzunluk = dizi.length;
+		for (int i = 0; i < uzunluk / 2; i++) {
+			int gecici = dizi[i];
+			dizi[i] = dizi[uzunluk - 1 - i];
+			dizi[uzunluk - 1 - i] = gecici;
+		}
+
+		return dizi;
+	}
+
+	public static int[] diziTersCevir3(int[] dizi) {
+
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < dizi.length; i++) {
+			stack.push(dizi[i]);
+		}
+		for (int i = 0; i < dizi.length; i++) {
+			dizi[i] = stack.pop();
+		}
+
+		return dizi;
 	}
 
 	public static void diziYazdir(int[] dizi) {
--- Ders02/src/EslesenElemanlarOrnegi.java
+++ Ders02/src/EslesenElemanlarOrnegi.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 public class EslesenElemanlarOrnegi {
-	
+
 	public static void main(String[] args) {
-		
+
 		int[] dizi1 = {1, 2, 3, 9, 5};
 		int[] dizi2 = {3, 4, 5, 6, 7};
 
 		System.out.print("Eşleşen elemanlar: ");
 		eslesenleriBulVeYazdir(dizi1, dizi2);
+		
+		eslesenleriBulVeYazdir2(dizi1, dizi2);
 	}
 
 	public static void eslesenleriBulVeYazdir(int[] dizi1, int[] dizi2) {
@@ -18,5 +22,29 @@ public class EslesenElemanlarOrnegi {
 				}
 			}
 		}
+	}
+
+	public static void eslesenleriBulVeYazdir2(int[] dizi1, int[] dizi2) {
+		Arrays.sort(dizi1);
+		Arrays.sort(dizi2);
+
+		System.out.print("Eşleşen Elemanlar: ");
+
+		int i = 0;
+		int j = 0;
+
+		while (i < dizi1.length && j < dizi2.length) {
+			if (dizi1[i] == dizi2[j]) {
+				System.out.print(dizi1[i] + " ");
+				i++;
+				j++;
+			} else if (dizi1[i] < dizi2[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+
+		System.out.println(); // Satırı bir alt satıra geçmek için
 	}
 }
