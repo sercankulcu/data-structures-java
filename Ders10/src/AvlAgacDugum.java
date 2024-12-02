485005d65ebaf84a8f0083c74379f45f62847cb7
Sercan Külcü <sercankulcu@gmail.com>
2024-12-03 00:48:13 +0300

    Update AvlAgacDugum.java

--- Ders10/src/AvlAgacDugum.java
+++ Ders10/src/AvlAgacDugum.java
@@ -2,20 +2,20 @@
 // AVL Ağacı düğüm sınıfı, ağaç yapısının temel yapı taşlarını oluşturur.
 public class AvlAgacDugum {
 
-  // Ağaçtaki düğümün değerini temsil eden anahtar değeri
-  int anahtar;
+	// Ağaçtaki düğümün değerini temsil eden anahtar değeri
+	int anahtar;
 
-  // Düğümün sol çocuğunu temsil eden referans
-  AvlAgacDugum solCocuk;
+	// Düğümün sol çocuğunu temsil eden referans
+	AvlAgacDugum solCocuk;
 
-  // Düğümün sağ çocuğunu temsil eden referans
-  AvlAgacDugum sagCocuk;
+	// Düğümün sağ çocuğunu temsil eden referans
+	AvlAgacDugum sagCocuk;
 
-  // AVL ağacında kullanılan yükseklik bilgisini saklayan değişken
-  int yukseklik;
+	// AVL ağacında kullanılan yükseklik bilgisini saklayan değişken
+	int yukseklik;
 
-  // Yapıcı metod: Belirtilen anahtar değeriyle bir AVL ağacı düğümü oluşturur
-  public AvlAgacDugum(int anahtar) {
-    this.anahtar = anahtar;
-  }
+	// Yapıcı metod: Belirtilen anahtar değeriyle bir AVL ağacı düğümü oluşturur
+	public AvlAgacDugum(int anahtar) {
+		this.anahtar = anahtar;
+	}
 }
